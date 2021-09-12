8051cc33b61167cec0e8628fbf5fe3754e5cbc47
VaryaTerennik <varvara.terennik@gmail.com>
2021-09-12 17:36:20 +0300

    main

--- backend/app.js
+++ backend/app.js
@@ -124,5 +124,124 @@ fnGetInformation('/people', "https://swapi.dev/api/people/");
 fnGetInformation('/planets', "https://swapi.dev/api/planets/");
 fnGetInformation('/starships', "https://swapi.dev/api/starships/");
 
+app.get('/people/:id', function (request, response) {
+  let sId = request.params.id
+  axios({
+    method: 'get',
+    url: `https://swapi.dev/api/people/${sId}`
+  }).then((oResponse) => {
+    let oPersonData = oResponse.data
 
+    let fnGetInfo = (sUrl) => {
+      return axios({
+        method: 'get',
+        url: sUrl
+      })
+    }
+
+    let filmsData = oPersonData.films.map(fnGetInfo)
+    let starshipsData = oPersonData.starships.map(fnGetInfo)
+    let homePlanet = axios({
+      method: 'get',
+      url: oPersonData.homeworld
+    })
+
+    Promise.all([Promise.all(filmsData), Promise.all(starshipsData), homePlanet]).then((aData)=> {
+      console.log(aData[2].data);
+      oPersonData.homeworld = aData[2].data
+      oPersonData.films = aData[0].map(el=>el.data)
+      oPersonData.starships = aData[1].map(el=>el.data)
+      response.send(oPersonData)
+    })
+    
+  });
+})
+
+app.get('/starships/:id', function (request, response) {
+  let sId = request.params.id
+  axios({
+    method: 'get',
+    url: `https://swapi.dev/api/starships/${sId}`
+  }).then((oResponse) => {
+    let oStarshipData = oResponse.data
+
+    let fnGetInfo = (sUrl) => {
+      return axios({
+        method: 'get',
+        url: sUrl
+      })
+    }
+
+    let filmsData = oStarshipData.films.map(fnGetInfo)
+
+    Promise.all([Promise.all(filmsData)]).then((aData)=> {
+      console.log(aData[0].data);
+      oStarshipData.films = aData[0].map(el=>el.data)
+      response.send(oStarshipData)
+    })
+    
+  });
+})
+
+app.get('/films/:id', function (request, response) {
+  let sId = request.params.id
+  axios({
+    method: 'get',
+    url: `https://swapi.dev/api/films/${sId}`
+  }).then((oResponse) => {
+    let oFilmsData = oResponse.data
+
+    let fnGetInfo = (sUrl) => {
+      return axios({
+        method: 'get',
+        url: sUrl
+      })
+    }
+
+    let charactersData = oFilmsData.characters.map(fnGetInfo)
+    let planetsData = oFilmsData.planets.map(fnGetInfo)
+    let starshipsData = oFilmsData.starships.map(fnGetInfo)
+
+    Promise.all([Promise.all(planetsData), Promise.all(starshipsData), Promise.all(charactersData)]).then((aData)=> {
+      console.log(aData[2].data);
+      oFilmsData.planets = aData[0].map(el=>el.data)
+      oFilmsData.starships = aData[0].map(el=>el.data)
+      oFilmsData.characters = aData[1].map(el=>el.data)
+      response.send(oFilmsData)
+    })
+    
+  });
+})
+
+
+app.get('/films/:id', function (request, response) {
+  let sId = request.params.id
+  axios({
+    method: 'get',
+    url: `https://swapi.dev/api/films/${sId}`
+  }).then((oResponse) => {
+    let oFilmsData = oResponse.data
+
+    let fnGetInfo = (sUrl) => {
+      return axios({
+        method: 'get',
+        url: sUrl
+      })
+    }
+
+    let charactersData = oFilmsData.characters.map(fnGetInfo)
+    let planetsData = oFilmsData.planets.map(fnGetInfo)
+    let starshipsData = oFilmsData.starships.map(fnGetInfo)
+
+    Promise.all([Promise.all(planetsData), Promise.all(starshipsData), Promise.all(charactersData)]).then((aData)=> {
+      console.log(aData[2].data);
+      oFilmsData.planets = aData[0].map(el=>el.data)
+      oFilmsData.starships = aData[0].map(el=>el.data)
+      oFilmsData.characters = aData[1].map(el=>el.data)
+      response.send(oFilmsData)
+    })
+    
+  });
+})
+  
 app.listen(3000)
